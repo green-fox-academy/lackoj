cb5ea885fe526e67450d002bfd656713b3152d77
Laszlo Juhasz <ifj.juhasz.laszlo@gmail.com>
2018-10-29 11:07:34 +0100

    exercise hello-others

---
+++ Week-02/Day-01/hello-others.ts
@@ -0,0 +1,9 @@
+// Greet 3 of your classmates with this program, in three separate lines like:
+//
+// Hello, Esther!
+// Hello, Mary!
+// Hello, Joe!
+
+console.log("Hello, Julcsi");
+console.log("Hello, Emese");
+console.log("Hello, Andras");
