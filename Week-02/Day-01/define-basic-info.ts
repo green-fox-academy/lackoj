0eb7cf2b479d487c5206a09ee5ac3e482ab253c4
Laszlo Juhasz <ifj.juhasz.laszlo@gmail.com>
2018-10-29 12:31:57 +0100

    exercise define-basic-info

---
+++ Week-02/Day-01/define-basic-info.ts
@@ -0,0 +1,17 @@
+// Define several things as a variable, then print their values
+// Your name as a string
+// Your age as an integer
+// Your height in meters as a float
+// Whether you are married or not as a boolean
+
+let myName: string = "Laszlo";
+console.log(myName);
+
+let myAge: number = 29;
+console.log(myAge);
+
+let myHeight: number = 1.87;
+console.log(myHeight);
+
+let Marriage: boolean = false;
+console.log(Marriage);
