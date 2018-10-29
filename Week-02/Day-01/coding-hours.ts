d295c12696047f2096125d48fe163bc5782b0586
Laszlo Juhasz <ifj.juhasz.laszlo@gmail.com>
2018-10-29 11:46:02 +0100

    exercise coding-hours

---
+++ Week-02/Day-01/coding-hours.ts
@@ -0,0 +1,13 @@
+// An average Green Fox attendee codes 6 hours daily
+// The semester is 17 weeks long
+//
+// Print how many hours is spent with coding in a semester by an attendee,
+// if the attendee only codes on workdays.
+//
+// Print the percentage of the coding hours in the semester if the average
+// work hours weekly is 52
+
+console.log("Avg. coding hours in a semester:");
+console.log(17 * 5 * 6);
+console.log("Percentage spent with coding of avg weekly working hours");
+console.log((30 / 52) * 100);
