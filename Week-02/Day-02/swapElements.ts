75bbdec4b8f37f0773e9f6ddacddeca921897a08
Laszlo Juhasz <ifj.juhasz.laszlo@gmail.com>
2018-10-30 14:05:47 +0100

    exercise swapElements

---
+++ Week-02/Day-02/swapElements.ts
@@ -0,0 +1,11 @@
+"use strict";
+export {};
+
+// -  Create a variable named `abc` with the following content: `["Arthur", "Boe", "Chloe"]`
+// -  Swap the first and the third element of `abc`
+
+let abc: string[] = ["Arthur", "Boe", "Chloe"];
+let a = abc[0];
+abc[0] = abc[2];
+abc[2] = a;
+console.log(abc);
