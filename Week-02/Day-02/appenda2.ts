b8ab15b130f80c25ed0b355df514f2f957c7eac9
Laszlo Juhasz <ifj.juhasz.laszlo@gmail.com>
2018-10-30 16:45:47 +0100

    exercise appenda2

--- Week-02/Day-02/appenda2.ts
+++ Week-02/Day-02/appenda2.ts
@@ -5,8 +5,18 @@ export {};
 //    with the following content: `["koal", "pand", "zebr"]`
 // -  Add all elements an `"a"` at the end
 
-let animals = ["koal", "pand", "zebr"];
-animals.forEach(function(element, index, array) {
-  array[index] = element + "a";
-});
+// let animals = ["koal", "pand", "zebr"];
+
+// animals.forEach(function(element) {
+//   element = element + "a";
+// });
+// console.log(animals);
+
+// animals = animals.map(function(e, i, a) {
+//   e = e + "a";
+//   return a[i];
+// });
+// console.log(animals);
+
+let animals = ["koal", "pand", "zebr"].map(el => el + "a");
 console.log(animals);
