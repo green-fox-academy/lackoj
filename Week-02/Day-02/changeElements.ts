884cd06324cf7337fad34755c03bf418ebea95d7
Laszlo Juhasz <ifj.juhasz.laszlo@gmail.com>
2018-10-30 16:02:27 +0100

    exercise changeElements

---
+++ Week-02/Day-02/changeElements.ts
@@ -0,0 +1,16 @@
+"use strict";
+export {};
+
+// -  Create an array named `numList` with the following content: `[1, 2, 3, 8, 5, 6]`
+// -  Change the 8 to 4 with the `.map` method
+// -  Print the fourth element as a test
+
+let numList: number[] = [1, 2, 3, 8, 5, 6];
+numList = numList.map(function(e, i, array) {
+  if (e === 8) {
+    return 4;
+  } else {
+    return e;
+  }
+});
+console.log(numList);
