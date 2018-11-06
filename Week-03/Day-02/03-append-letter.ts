a5c327a95b172d5c7fb1467469694ec10fb51056
Laszlo Juhasz <ifj.juhasz.laszlo@gmail.com>
2018-11-06 17:18:15 +0100

    add solution of Append letters

---
+++ Week-03/Day-02/03-append-letter.ts
@@ -0,0 +1,18 @@
+"use strict";
+export {};
+
+// Create a function called "appendA()" that adds "a" to every string in the far list.
+// The parameter should be a list.
+
+let far: string[] = ["bo", "anacond", "koal", "pand", "zebr"];
+
+function appendA(arr: string[]) {
+  arr.forEach(function(element, i, arr) {
+    arr[i] = element + "a";
+  });
+  return arr;
+}
+
+console.log(appendA(far));
+
+// The output should be: 'boa', 'anaconda', 'koala', 'panda', 'zebra'
