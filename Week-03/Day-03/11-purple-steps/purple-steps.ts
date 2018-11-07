ec7486ba03dd4d6caa6212c175e49ee9fecb25e9
Laszlo Juhasz <ifj.juhasz.laszlo@gmail.com>
2018-11-07 14:55:00 +0100

    add solution of Purple-steps

---
+++ Week-03/Day-03/11-purple-steps/purple-steps.ts
@@ -0,0 +1,14 @@
+"use strict";
+
+const canvas = document.querySelector(".main-canvas") as HTMLCanvasElement;
+const ctx = canvas.getContext("2d");
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+
+// Reproduce this:
+// [https://github.com/green-fox-academy/teaching-materials/blob/master/workshop/drawing/purple-steps/r3.png]
+
+for (let i = 1; i < 20; i++) {
+  ctx.fillStyle = "purple";
+  ctx.fillRect(10 * i, 10 * i, 10, 10);
+}
