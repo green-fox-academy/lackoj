6a6839e2ca0e90743b47237e0754df7bfcd3f52a
Laszlo Juhasz <ifj.juhasz.laszlo@gmail.com>
2018-11-12 14:33:44 +0100

    add solution of 04-write-single-line

---
+++ Week-04/Day-01/04-write-single-line.ts
@@ -0,0 +1,16 @@
+// Open a file called 'my-file.txt'
+// Write your name in it as a single line
+// If the program is unable to write the file,
+// then it should print an error message like: 'Unable to write file: my-file.txt'
+
+const fs = require("fs");
+
+function writeToFile(fileName: string, data: string): string {
+  if (fs.existsSync(fileName)) {
+    return fs.writeFileSync(fileName, data);
+  } else {
+    console.log("Unable to write file: my-file.txt");
+  }
+}
+
+writeToFile("myfile.txt", "Juhaaasz");
