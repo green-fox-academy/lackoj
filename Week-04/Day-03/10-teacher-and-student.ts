045e0990f2d142f307b9fc106bff477d1a6f920f
Laszlo Juhasz <ifj.juhasz.laszlo@gmail.com>
2018-11-14 12:47:23 +0100

    add solution of 10-teacher-and-student

---
+++ Week-04/Day-03/10-teacher-and-student.ts
@@ -0,0 +1,44 @@
+'use strict';
+export { };
+
+/* Create Student and Teacher classes
+Student
+learn()
+question(teacher) -> calls the teachers answer method
+Teacher
+teach(student) -> calls the students learn method
+answer() */
+
+class Student {
+
+  knowledge: string[];
+
+  question(teacher: Teacher) {
+    console.log("Student: Can you tell me...?")
+    teacher.answer();
+  }
+
+  learn() {
+    console.log("Student: Thank you! Now I understand.")
+  }
+
+}
+
+class Teacher {
+
+  teach(student: Student) {
+    console.log("Teacher: It is very important to....")
+    student.learn();
+  }
+
+  answer() {
+    console.log("Teacher: Have you googled it?")
+  }
+}
+
+let joe = new Student();
+let mrsLockwood = new Teacher();
+
+joe.question(mrsLockwood);
+console.log("-----------------")
+mrsLockwood.teach(joe);
