d381286aca278345017cfb866514fe43ec682571
Laszlo Juhasz <ifj.juhasz.laszlo@gmail.com>
2018-11-14 17:05:11 +0100

    add 14-blog.ts - not solved

---
+++ Week-04/Day-03/14-blog.ts
@@ -0,0 +1,23 @@
+import { BlogPost } from '../Day-02/02-blogpost';
+'use strict';
+
+/* 
+Reuse your BlogPost class
+Create a Blog class which can store a list of BlogPosts
+add BlogPosts to the list
+delete(int) one item at given index
+update(int, BlogPost) one item at the given index and update it with another BlogPost
+*/
+
+class Blog {
+  posts: BlogPost[] = [];
+
+  getIindexOf() { }
+
+  delete(index: number) {
+  }
+
+  udpate(index: number, anotherPost: BlogPost) {
+  }
+
+}
