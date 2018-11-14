ee524a713b10b88fe80c636a4ecef1ffcbe44b32
Laszlo Juhasz <ifj.juhasz.laszlo@gmail.com>
2018-11-14 12:13:50 +0100

    add solution of 11-petrol-station

---
+++ Week-04/Day-03/11-petrol-station.ts
@@ -0,0 +1,42 @@
+/* Create a Station and a Car classes
+Station
+gasAmount
+refill(car) -> decreases the gasAmount by the capacity of the car and increases the cars gasAmount
+Car
+gasAmount
+capacity
+create constructor for Car where:
+initialize gasAmount -> 0
+initialize capacity -> 100 */
+
+class Station {
+  gasAmount: number = 1000;
+
+  refill(car: Car): void { //decreases the gasAmount by the capacity of the car and increases the cars gasAmount
+    this.gasAmount -= car.capacity;
+    car.gasAmount += car.capacity;
+  }
+}
+
+class Car {
+  gasAmount: number;
+  capacity: number;
+
+  constructor(gasAmount: number = 0, capacity: number = 100) {
+    this.gasAmount = gasAmount;
+    this.capacity = capacity;
+  }
+}
+
+let station01 = new Station();
+let car01 = new Car();
+
+console.log(station01);
+console.log(car01);
+console.log("-------------------------");
+
+station01.refill(car01);
+console.log(station01);
+console.log(car01);
+
+
