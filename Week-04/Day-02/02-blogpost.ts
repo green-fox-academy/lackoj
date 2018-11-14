7fdc8b2fe9af2aa4bdf6974aab08e88cab6561fc
Laszlo Juhasz <ifj.juhasz.laszlo@gmail.com>
2018-11-14 16:43:39 +0100

    add export and small correction to 02-blogpost

--- Week-04/Day-02/02-blogpost.ts
+++ Week-04/Day-02/02-blogpost.ts
@@ -14,7 +14,7 @@ Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the center 
 "use strict";
 export { };
 
-class blogPost {
+export class BlogPost {
   authorName: string;
   title: string;
   text: string;
@@ -28,10 +28,10 @@ class blogPost {
   }
 }
 
-let post01 = new blogPost("John Doe", "Lorem Ipsum", "Lorem ipsum dolor sit amet.", "2000.05.04.");
+let post01 = new BlogPost("John Doe", "Lorem Ipsum", "Lorem ipsum dolor sit amet.", "2000.05.04.");
 
-let post02 = new blogPost("Tim Urban", "Wait but why", "A popular long-form, stick-figure-illustrated blog about almost everything.", "2010.10.10.");
+let post02 = new BlogPost("Tim Urban", "Wait but why", "A popular long-form, stick-figure-illustrated blog about almost everything.", "2010.10.10.");
 
-let post03 = new blogPost("William Turton", "One Engineer Is Trying to Get IBM to Reckon With Trump", "Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the center of attention. When I asked to take his picture outside one of IBM’s New York City offices, he told me that he wasn’t really into the whole organizer profile thing.", "2017.03.28.");
+let post03 = new BlogPost("William Turton", "One Engineer Is Trying to Get IBM to Reckon With Trump", "Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the center of attention. When I asked to take his picture outside one of IBM’s New York City offices, he told me that he wasn’t really into the whole organizer profile thing.", "2017.03.28.");
 
 console.log(post01);
