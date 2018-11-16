4ffc86bcaf71fc259a05a30e8ac66b882544bec5
Laszlo Juhasz <ifj.juhasz.laszlo@gmail.com>
2018-11-16 14:18:17 +0100

    add solution of fractal - triangles

---
+++ Week-04/Day-05/Triangles/triangles.ts
@@ -0,0 +1,99 @@
+"use strict";
+
+const canvas = document.querySelector(".main-canvas") as HTMLCanvasElement;
+const ctx = canvas.getContext("2d");
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+
+'use strict';
+
+function height(side) {
+  return (side * Math.sqrt(3)) / 2;
+}
+
+function drawtriangleNew(x: number, y: number, sideLength: number) {
+
+  ctx.strokeStyle =
+    "#" + (Math.random().toString(16) + "000000").substring(2, 8);
+
+  //first left triangle
+  ctx.beginPath();
+  ctx.moveTo(x, y);
+  ctx.lineTo(x + sideLength / 2, y + height(sideLength));
+  ctx.lineTo(x + sideLength, y);
+  ctx.closePath();
+  ctx.stroke();
+
+  //first right triangle
+  ctx.beginPath();
+  ctx.moveTo(x + sideLength, y);
+  ctx.lineTo(x + sideLength + sideLength / 2, y + height(sideLength));
+  ctx.lineTo(x + sideLength * 2, y);
+  ctx.closePath();
+  ctx.stroke();
+
+  //top 
+  ctx.beginPath();
+  ctx.moveTo(x + sideLength / 2, y + height(sideLength));
+  ctx.lineTo(x + sideLength, y + height(sideLength * 2));
+  ctx.lineTo(x + sideLength + sideLength / 2, y + height(sideLength));
+  ctx.closePath();
+  ctx.stroke();
+
+}
+
+function drawFractalTriangles(x, y, size, i) {
+  drawtriangleNew(x, y, size);
+
+  if (size > 10) {
+    drawFractalTriangles(x, y, size / 2, i - 1);
+    drawFractalTriangles(x + size, y, size / 2, i - 1);
+    drawFractalTriangles(x + size / 2, y + height(size), size / 2, i - 1);
+  }
+}
+
+drawFractalTriangles(0, 0, 200, 3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* ---------------------------------------------------------------------------------------------- */
+
+function drawTriangles(size, lineDistance) {
+  let distance = size;
+
+  ctx.translate((canvas.width - size) / 2, 0);
+
+  for (let col = 0; col < size; col += lineDistance) {
+    for (let row = 0; row < distance; row += lineDistance) {
+      let cSide = triangleHeight(lineDistance);
+      let coordY = size - triangleHeight(col);
+
+      ctx.strokeStyle = "blue";
+
+      ctx.beginPath();
+      ctx.moveTo(row + col / 2, coordY);
+      ctx.lineTo(lineDistance / 2 + row + col / 2, coordY - cSide);
+      ctx.lineTo(lineDistance + row + col / 2, coordY);
+      ctx.closePath();
+      ctx.stroke();
+    }
+    distance -= lineDistance;
+  }
+}
+// drawTriangles(400, 100);
