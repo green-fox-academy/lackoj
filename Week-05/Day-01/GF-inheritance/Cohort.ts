d8a0e5cb451cf4752aa89a0391274b9441a6492e
Laszlo Juhasz <ifj.juhasz.laszlo@gmail.com>
2018-11-19 12:20:16 +0100

    feat(GF-inheritance): create Cohort class in separate file

--- Week-05/Day-01/GF-inheritance/Cohort.ts
+++ Week-05/Day-01/GF-inheritance/Cohort.ts
@@ -1,1 +1,22 @@
 "use strict";
+import { Student } from './Student';
+import { Mentor } from './Mentor';
+
+class Cohort {
+
+  name: string;  //the name of the cohort
+  students: Student[] = []; //a list of Students
+  mentors: Mentor[] = []; //a list of Mentors
+
+  addStudent(student: Student) {// adds the given Student to students list
+    this.students.push(student);
+  }
+
+  addMentor(mentor: Mentor) { //adds the given Mentor to mentors list
+    this.mentors.push(mentor);
+  }
+
+  info() { //prints out 'The name cohort has students.size students and mentors.size mentors.'
+    console.log(`The ${this.name} has ${this.students.length} students and ${this.mentors.length} mentors.`)
+  }
+}
