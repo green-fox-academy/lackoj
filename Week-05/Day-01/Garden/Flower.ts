f05017e257c30995889854622adf6d7383f5c05c
Laszlo Juhasz <ifj.juhasz.laszlo@gmail.com>
2018-11-19 15:35:45 +0100

    feat(Garden): Flower.ts

---
+++ Week-05/Day-01/Garden/Flower.ts
@@ -0,0 +1,15 @@
+import { Plant } from "./Plant";
+
+'use strict';
+
+/*The Flower
+needs water if its current water amount is less then 5
+when watering it the flower can only absorb the 75% of the water
+eg. watering with 10 the flower's amount of water should only increase with 7.5*/
+
+export class Flower extends Plant {
+  constructor(color: string, minWaterAmountNeed = 5, ableToAbsorve = 0.75) {
+    super(color, minWaterAmountNeed, ableToAbsorve);
+    this.currentWaterAmount = 0;
+  }
+}
