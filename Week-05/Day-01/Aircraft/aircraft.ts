3dcaa865bce96f1ae880a11d28e412e39cd0b12e
Laszlo Juhasz <ifj.juhasz.laszlo@gmail.com>
2018-11-21 08:57:45 +0100

    feat(Aircraft): add methods to Aircraft class

--- Week-05/Day-01/Aircraft/aircraft.ts
+++ Week-05/Day-01/Aircraft/aircraft.ts
@@ -28,13 +28,8 @@ class Carrier {
 
 abstract class Aircraft {
 
-  // ezekre itt elvileg nincs szükség? 
-  protected ammo: number;
-  protected maxAmmo: number;
-  protected baseDamage: number;
-  protected type: string;
-
-  constructor(maxAmmo: number, baseDamage: number, ammo = 0) {
+  protected constructor(protected maxAmmo: number, protected baseDamage: number, protected ammo = 0) {
+    this.baseDamage = baseDamage;
     this.baseDamage = baseDamage;
     this.maxAmmo = maxAmmo;
     this.ammo = ammo;
@@ -42,7 +37,10 @@ abstract class Aircraft {
 
   // It should use all the ammos (set it to 0) and it should return the damage it deals
   // The damage is the multiplication of the base damage and the ammos
-  fight() { }
+  fight() {
+    this.ammo = 0;
+    console.log(this.baseDamage); // talan kell egy masik damage is
+  }
 
   /*
   It should take a number as parameter and substract as much ammo as possibe
@@ -50,32 +48,45 @@ abstract class Aircraft {
   It should return the remaining ammo
   Eg. Filling an empty F35 with 300 would completely fill the storage of the aircraft and would return the remaining 288
   */
-  reFill() { }
+  reFill(reFillAmount: number): number {
+    if (this.maxAmmo < reFillAmount) {
+      this.ammo += this.maxAmmo;
+      return reFillAmount - this.maxAmmo
+    } else if (this.maxAmmo > reFillAmount) { //ez a condition nem kell csak az else sem igazabol, delete later
+      this.ammo += reFillAmount;
+      return 0;
+    };
+  }
 
-  //it should return it's type as a string
-  getType() { }
+  //it should return it's type as a string -->typeof? instaceof?
+  getType() {
+    return this.constructor.name;
+  }
 
   //It should return a string like: Type F35, Ammo: 10, Base Damage: 50, All Damage: 500
-  getStatus() { }
+  getStatus() { //kell egy all damage valahova
+    return (`Type : ${this.constructor.name}, Ammo: ${this.ammo}, Base Damage: ${this.baseDamage}`)
+  }
 
   //It should return if the aircraft is priority in the ammo fill queue. It's true for F35 and false for F16
-  isPriority() { }
+  isPriority() { //check hogy mit ad ki a contructor.name ebben az esetben, instancon tesztelni a fv-t ha kész
+    if (this.constructor.name === "F35") {
+      return true;
+    }
+    return false;
+  }
 
 }
 
 class F16 extends Aircraft {
-
   constructor(maxAmmo = 8, baseDamage = 30, ammo = 0) {
     super(maxAmmo, baseDamage, ammo);
   }
-
 }
 
 class F35 extends Aircraft {
-
   constructor(maxAmmo = 12, baseDamage = 50, ammo = 0) {
     super(maxAmmo, baseDamage, ammo);
   }
-
 }
 
