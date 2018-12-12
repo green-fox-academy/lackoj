e8689404b707472bc028b73406fb1176c104c31d
Laszlo Juhasz <ifj.juhasz.laszlo@gmail.com>
2018-12-12 16:12:19 +0100

    feat(click-three-times): add solution

---
+++ Week-08/Day-03/Exercise-06/cick-three-times.js
@@ -0,0 +1,17 @@
+// If the user clicks the button 3 times, and 5 seconds is already elapsed since the page is loaded, a text should appear under the button:
+// 5 seconds elapsed and clicked 3 times
+// If the user starts clicking before the first 5 seconds, nothing should be printed
+
+let btn = document.querySelector("button");
+let count = 0;
+
+let myFunction = (e) => {
+  if (e.timeStamp < 5000) {
+    btn.removeEventListener("click", myFunction);
+    console.log("Eventlistener has been removed.");
+  } else if (count >= 3 && e.timeStamp > 5000) {
+    document.querySelector("p").innerText = "5 seconds elapsed and clicked 3 times";
+  }
+  count++;
+}
+btn.addEventListener("click", myFunction);
