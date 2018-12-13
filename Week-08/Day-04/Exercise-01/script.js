00dfd176569a55686135abd9750cd0ac96537e19
Laszlo Juhasz <ifj.juhasz.laszlo@gmail.com>
2018-12-13 11:41:41 +0100

    feat(login-from): add files

---
+++ Week-08/Day-04/Exercise-01/script.js
@@ -0,0 +1,9 @@
+const submit = document.querySelector("button");
+const link = document.querySelector(".forgot-password")
+
+submit.addEventListener("click", (event) => {
+  event.preventDefault();
+});
+link.addEventListener("click", (event) => {
+  event.preventDefault();
+});
