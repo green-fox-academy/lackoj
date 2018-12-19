cb1dcaae14727e40637736be1256b8cd5c54f6a7
Laszlo Juhasz <ifj.juhasz.laszlo@gmail.com>
2018-12-19 12:37:00 +0100

    feat(rest backend): doubling

--- Week-09/Day-03/Project-01/app.js
+++ Week-09/Day-03/Project-01/app.js
@@ -10,6 +10,19 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+app.get('/doubling', (req, res) => {
+  const { input } = req.query; //= req.query.input
+  if (input) {
+    const resObj = {
+      'received': input,
+      'result': input * 2
+    }
+    res.json(resObj);
+  } else {
+    res.json({ 'error': "Please provide an input!" });
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Listening on PORT ${PORT}`);
 });
