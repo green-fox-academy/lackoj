15239c2178da7a5d33a003ec23c3f6a20a97e276
Laszlo Juhasz <ifj.juhasz.laszlo@gmail.com>
2018-12-19 12:36:16 +0100

    feat(rest backend): initial commit

---
+++ Week-09/Day-03/Project-01/app.js
@@ -0,0 +1,15 @@
+"use strict";
+const express = require("express");
+const app = express();
+const path = require('path');
+const PORT = 8080;
+
+app.use('/assets', express.static('assets'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on PORT ${PORT}`);
+});
