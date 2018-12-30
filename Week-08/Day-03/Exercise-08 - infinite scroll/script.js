d8db932f6752fe0a80e443ae5c5c810cb83d9b10
Laszlo Juhasz <ifj.juhasz.laszlo@gmail.com>
2018-12-30 11:23:39 +0100

    refact(infinite scroll): function structure update

--- Week-08/Day-03/Exercise-08 - infinite scroll/script.js
+++ Week-08/Day-03/Exercise-08 - infinite scroll/script.js
@@ -1,23 +1,29 @@
 'use strict';
 
-const body = document.querySelector('body');
-const scrollThreshold = 300;
-
-let randomNumber = Math.floor((Math.random() * 10) + 1);
-
 const createDivs = (howMany) => {
+  const body = document.querySelector('body');
   for (let i = 0; i <= howMany; i++) {
-    let div = document.createElement('div');
-    let randomColor = '#' + Math.random().toString(16).substr(-6);
+    const div = document.createElement('div');
+    const randomColor = '#' + Math.random().toString(16).substr(-6);
     div.style.backgroundColor = randomColor;
     body.appendChild(div);
   };
 };
 
+function infiniteScroll(scrollThreshold, callback) {
+  window.addEventListener('scroll', () => {
+    const currentPosition = window.scrollY + window.innerHeight;
+    const refreshPosition = document.body.offsetHeight - scrollThreshold;
+
+    if (currentPosition >= refreshPosition) {
+      callback();
+    }
+  });
+}
+
+const randomNumber = Math.floor((Math.random() * 10) + 1);
 createDivs(randomNumber);
 
-window.addEventListener('scroll', () => {
-  if ((window.scrollY + window.innerHeight) >= (document.body.offsetHeight - scrollThreshold)) {
-    createDivs(10);
-  }
+infiniteScroll(300, () => {
+  createDivs(10);
 });
