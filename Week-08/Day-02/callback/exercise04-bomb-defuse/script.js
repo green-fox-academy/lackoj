f1f5c66943f4da7f642a5043b19d35ef2ed3172a
Laszlo Juhasz <ifj.juhasz.laszlo@gmail.com>
2018-12-30 13:52:50 +0100

    feat(bomb-defuse): add solution

---
+++ Week-08/Day-02/callback/exercise04-bomb-defuse/script.js
@@ -0,0 +1,28 @@
+'use strict';
+
+// Create a timeout that will set the display to "Bomb exploded" in 10 seconds
+// If you click on the button set the display to "Bomb defused" and stop the timeout
+// Extra: create an interval which will update the remaining seconds in the display
+
+const display = document.querySelector('.display');
+const button = document.querySelector('button');
+let count = 9;
+
+
+const updateRemainingSec = setInterval(() => {
+  if (count > 0) {
+    display.innerText = count;
+    count--;
+  } else {
+    clearInterval(updateRemainingSec);
+    display.innerText = "Bomb exploded";
+    button.setAttribute('disabled', true);
+  }
+}, 500);
+
+button.addEventListener('click', () => {
+  if (count !== 0) {
+    display.innerText = "Bomb defused";
+    clearInterval(updateRemainingSec);
+  }
+});
