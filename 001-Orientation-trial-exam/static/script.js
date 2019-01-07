85c5d412c2322410d7ee3df22c275c9043f35e7e
Laszlo Juhasz <ifj.juhasz.laszlo@gmail.com>
2019-01-07 14:32:36 +0100

    feat(orientation trial): add sendHTTPrequest function

--- 001-Orientation-trial-exam/static/script.js
+++ 001-Orientation-trial-exam/static/script.js
@@ -1,2 +1,13 @@
 'use strics';
 
+const sendHTTPRequest = (url, method, callback) => {
+  const xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.onload = () => {
+    if (xhr.status === 200) {
+      callback(JSON.parse(xhr.responseText));
+    };
+  };
+  xhr.send();
+};
+
