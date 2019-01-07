e898ceb20b7e9fedcfab5abc898192ea88f4056f
Laszlo Juhasz <ifj.juhasz.laszlo@gmail.com>
2019-01-07 20:41:28 +0100

    feat(orientaion trial): finalize eventlistener

--- 001-Orientation-trial-exam/static/script.js
+++ 001-Orientation-trial-exam/static/script.js
@@ -1,13 +1,31 @@
-'use strics';
+'use strict';
 
-const sendHTTPRequest = (url, method, callback) => {
+const p = document.querySelector('p');
+const form = document.querySelector('form');
+const { url, alias } = form.elements;
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
   const xhr = new XMLHttpRequest();
-  xhr.open(method, url);
+  xhr.open('POST', "/api/links");
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.send(JSON.stringify({
+    url: url.value,
+    alias: alias.value
+  }));
+
   xhr.onload = () => {
-    if (xhr.status === 200) {
-      callback(JSON.parse(xhr.responseText));
+
+    if (xhr.status === 400) {
+      p.innerHTML = "";
+      p.innerText = 'Your alias is already in use!';
+      p.style.color = "red";
+    } else if (xhr.status === 200) {
+      p.innerHTML = "";
+      const { alias: newAlias, secretCode: newSecreCode } = JSON.parse(xhr.responseText);
+      p.innerHTML = `Your URL is aliased to <strong>"${newAlias}"</strong> and your secret code is <strong>"${newSecreCode}"</strong>`
+      p.style.color = "black";
+      form.reset();
     };
   };
-  xhr.send();
-};
-
+});
