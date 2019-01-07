bb5fe66eadc3ccd20a5cdc7e338dabbc54a96eb3
Laszlo Juhasz <ifj.juhasz.laszlo@gmail.com>
2019-01-07 15:28:01 +0100

    feat(orientation trial): set up server + connections

--- 001-Orientation-trial-exam/app.js
+++ 001-Orientation-trial-exam/app.js
@@ -1,1 +1,39 @@
-'use strics';
+'use strict';
+
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+const path = require("path");
+const PORT = 3000;
+
+//dotenv + express
+require('dotenv').config();
+app.use(express.json());
+
+//send html file to Localhost
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+//create connection 
+const mySqlConnection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD
+});
+
+// Check if connection works
+mySqlConnection.connect(function (err) {
+  if (err) {
+    console.log(err.message);
+    return;
+  };
+  console.log('Connection established');
+});
+mySqlConnection.end();
+
+//listen to PORT
+app.listen(PORT, () => {
+  console.log(`Listening on PORT ${PORT}`);
+});
