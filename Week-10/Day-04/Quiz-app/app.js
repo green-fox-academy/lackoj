f9e0dc1d453837beaea6d1bdffa9f221784989c6
Laszlo Juhasz <ifj.juhasz.laszlo@gmail.com>
2019-01-08 20:40:48 +0100

    feat(quiz): initial commit

---
+++ Week-10/Day-04/Quiz-app/app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+const path = require('path');
+const PORT = 3000;
+
+app.use(express.json());
+
+const conn = mysql.createConnection({
+  user: 'root',
+  password: 'password',
+  database: 'database', //check
+  port: 3306
+});
+
+conn.connect(function (err) {
+  if (err) {
+    console.log('Error connecting to Db');
+    return;
+  }
+  console.log('Connection established');
+});
+
+app.use('/static', express.static('static'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`App is listening on port ${PORT}`);
+});
