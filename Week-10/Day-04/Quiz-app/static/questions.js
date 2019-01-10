8d056fe19ceec3ff54d22854dd4413b16357e450
Laszlo Juhasz <ifj.juhasz.laszlo@gmail.com>
2019-01-10 14:15:30 +0100

    feat(quiz): questions.js: xhr + showQuestions function

--- Week-10/Day-04/Quiz-app/static/questions.js
+++ Week-10/Day-04/Quiz-app/static/questions.js
@@ -1,2 +1,29 @@
 'use strict';
 
+const questionContainer = document.querySelector(".questions-container");
+const answersContainer = document.querySelector(".answers-container");
+
+const sendHTTPRequest = (url, method, callback) => {
+  const xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.onload = () => {
+    if (xhr.status === 200) {
+      callback(JSON.parse(xhr.responseText));
+    }
+  }
+  xhr.send();
+};
+
+const showQestions = (data) => {
+  data.forEach(questionObj => {
+    const p = document.createElement("p");
+    const deleteBtn = document.createElement("button");
+    deleteBtn.dataset.id = questionObj.id;
+    deleteBtn.innerText = "delete";
+    p.textContent = questionObj.question;
+    questionContainer.appendChild(p);
+    questionContainer.appendChild(deleteBtn);
+  });
+};
+
+sendHTTPRequest('/api/questions', 'GET', showQestions);
